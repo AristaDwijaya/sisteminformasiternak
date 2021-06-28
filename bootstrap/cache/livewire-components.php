b91e2beced83caa138361dbe182911e266018d3e
AristaDwijaya <79678943+AristaDwijaya@users.noreply.github.com>
2021-06-28 16:55:35 +0800

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'cart' => 'App\\Http\\Livewire\\Cart',
+  'product' => 'App\\Http\\Livewire\\Product',
+  'product-transactions' => 'App\\Http\\Livewire\\ProductTransactions',
+  'transactions' => 'App\\Http\\Livewire\\Transactions',
+  'types' => 'App\\Http\\Livewire\\Types',
+);
