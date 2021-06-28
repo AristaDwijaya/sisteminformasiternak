d6b70e70cbd39b64ba28fea426652eeef6a9d78b
AristaDwijaya <79678943+AristaDwijaya@users.noreply.github.com>
2021-06-28 17:00:43 +0800

    Add files via upload

---
+++ routes/resources/views/dashboard.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+    <div class="jumbotorn text-center">
+        <img src="Screenshot_1.png" width="600">
+
+        <div class="text-left">
+            <h3>About Us</h3>
+            <h6>Pengertian Ternak</h6>
+            <h6>Kegiatan ternak pada dasarnya terbagi menjadi dua golongan yakni peternakan hewan besar seperti
+                sapi, kerbau dan kuda sedangkan untuk kelompok kedua yakni peternakan hewan kecil seperti ayam,
+                kelinci, bebek, angsa dan hewan ternak lainnya.</h6>
+            <h6>Pengertian Ternak Menurut Para Ahli.</h6>
+            <h6>1. KBBI (2015), definisi ternak adalah berbagai bentuk binatang yang sengaja untuk dipelihara dan
+                diperkembangbiakan dengan tujuan untuk mendapatkan hasil melalui produksi dan pemeliharaan yang
+                dilakukan..</h6>
+            <h6>2. Aksi Agraris Kanisius (1978), Arti perterenakan adalah hewan yang hidup di bawah pengawasan manusia
+                dan
+                bisa hidup dengan pengawasan manusia, sehingga ketergentungan dan hasil yang diperoleh nantinya juga
+                untuk memenuhi kebutuhan pemelihara (manusia)..</h6>
+            <h6>3. Samad Sosroamidjoyo (1978), Berternak adalah segenap usaha yang dilakukan seseorang dalam memelihara
+                dan mengambengbiakan hewan dengan tujuan mendapatkan keuntungan ekonomis..</h6>
+            <h6>Jenis Ternak
+                Macam-macam ternak yang dilakukan oleh manusia terbagi atas berbagai jenis, antara lain;
+            </h6>
+
+            <img src="sapi.jpg" alt="sapi" width="500">
+            <h6>* Ternak Hewan Besar
+                Ternak jenis ini yaitu suatu kegiatan memelihara atau membudidayakan hewan-hewan yang memiliki tubuh
+                besar. Contoh ternak hewan besar ini seperti :
+            </h6>
+
+            <h6>- Sapi</h6>
+            <h6>- Kuda</h6>
+            <h6>- Kerbau</h6>
+
+            <h6>Karakteristik ternak hewan ini memiliki tubuh yang cukup besar untuk dapat diambil manfaatnya dalam
+                bentuk susu, daging, kulit, ataupun bisa dengan tenaganya sebagai alat transportasi atau membajak.
+                Selain itu, kotorannya juga dapat digunakan sebagai pupuk alamiah yang dibutuhkan dalam usaha
+                kegiatan
+                pertanian dan perkebunan.
+            </h6>
+            <img src="kambing-dan-domba.jpg" alt="kambing-dan-domba" width="500">
+            <h6> * Ternak Hewan Kecil
+                Peternakan hewan kecil yaitu saatu cara memelihara dan mengembangbiakan hewan-hewan yang memiliki
+                tubuh agak kecil seperti :
+                <h6>
+                    - Babi
+                </h6>
+                <h6>
+                    - Kambing
+                </h6>
+                <h6>
+                    - Domba
+                </h6>
+                <h6>
+                    - Kelinci
+                </h6>
+                <h6>
+                    - dan lain sebagainya
+                </h6>
+                <h6> Manfaat beternak hewan yang
+                    memiliki tubuh kecil yakni untuk diambil susu, daging, serta kulitnya, selain itu bagi yang hanya
+                    memiliki modal minim beternak hewan kecil cukup diminati.
+                </h6>
+                <img src="unggas1.jpg" alt="unggas" width="500">
+                <h6>* Ternak Hewan Unggas</h6>
+                <h6>Ayam, bebek, angsa, itik, serta burung puyuh merupakan sebagian contoh hewan unggas yang banyak
+                    ditumbuhkembangkan oleh masyarakat. Manfaat dari suatu usaha ternak hewan unggas yaitu untuk diambil
+                    daging, telur, bulu, dan juga dapat dijadikan sebagai penghibur untuk dinikmati suara atau
+                    keindahannya.</h6>
+        </div>
+
+
+</x-app-layout>
